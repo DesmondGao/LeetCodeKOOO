2551a3aa63fa06e79f5fdfe343165f1c996eb6cb
DesmondGao <linchaogao@163.com>
2016-06-06 19:13:04 +0800

    Update twoSum.cpp

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,4 +1,5 @@
 
+//原始的直接暴力查找法
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
@@ -16,5 +17,37 @@ public:
           }
         }
     return holder;
+    }
+};
+
+//当然可以先排序，建议快排，然后再暴力查找原始数组里面和排序后数组一样的数值下标
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target){
+        vector<int> temp_nums=nums;
+        vector<int> ans;
+        sort(temp_nums.begin(),temp_nums.end());
+        int i=0; int j=temp_nums.size()-1;
+        while(i<j)
+        {
+            if(temp_nums[i]+temp_nums[j]==target)
+            {
+                //需要明确-->排序后的数字是以前数组的相同数组的那个下标
+                for(int k=0;k<nums.size();k++)
+                {
+                    if(temp_nums[i]==nums[k]||temp_nums[j]==nums[k])
+                    {
+                        ans.push_back(k);
+                    }
+                }
+                break;//break位置放不对会超时
+            }else if(temp_nums[i]+temp_nums[j]<target){
+               i++;
+            }else{
+                j--;
+            }
+        }
+        return ans;
     }
 };
