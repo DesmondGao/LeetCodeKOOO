0e37a13f84731b3d813c5f1ed55394b7e365e830
DesmondGao <linchaogao@163.com>
2016-06-06 21:25:00 +0800

    Update Remove Duplicates from Sorted Array.cpp

--- Remove Duplicates from Sorted Array.cpp
+++ Remove Duplicates from Sorted Array.cpp
@@ -1,6 +1,6 @@
 //既然不能占用额外空间，那就只能将后面(特别是same-series和后面的接触点(3)赋值)--》1 2 2 2 2 3 4
 //timer就是一个标记same-series起点的第二个点的一个标记
-//这个题本身就有点小问题，上面的case其返回值为1 2 3 4 2 
+//这个题本身就有点小问题，上面的case其返回值为1 2 3 4 2 ，竟然通过测试
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
