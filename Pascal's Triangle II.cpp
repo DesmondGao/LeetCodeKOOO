46519b98ae087407e3b800ecb72198be2968a8a3
DesmondGao <linchaogao@163.com>
2016-06-13 17:19:29 +0800

    杨辉三角的变种问题，获取指定行的值

---
+++ Pascal's Triangle II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row(0);
+        for(int i=0;i<rowIndex+1;i++)
+        {
+            row.push_back(1);
+            for(int j=i-1;j>0;j--)
+            {
+                row[j]=row[j-1]+row[j];
+            }
+        }
+        return row;
+    }
+};
