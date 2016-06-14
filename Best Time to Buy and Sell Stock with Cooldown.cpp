90292fabb86b1cd2546b49913d8e2bb37e03d06f
DesmondGao <linchaogao@163.com>
2016-06-14 16:42:10 +0800

    Update Best Time to Buy and Sell Stock with Cooldown.cpp
    1.主要是从函数的角度去思考；--->但计算结果不尽如人意。
    2.采用动态规划的思想，特别是用有股票和没有股票来计算最终抛售后的最大收益性问题

--- Best Time to Buy and Sell Stock with Cooldown.cpp
+++ Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -1,4 +1,4 @@
-//再思考----->
+//再思考----->self方法没有AC-->主要是想通过增减性找到峰值点，然后其冷却一天不影响我的收益，。。
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -27,5 +27,30 @@ public:
             }
         }
         return maxProfit;
+    }
+};
+
+//动态规划思想
+class Solution {
+public:
+    int maxProfit(vector<int>& prices){
+        //采用动态规划计算
+        //主要是怎样设定动态方程-->股票的买与卖，即手头有股票和没有股票
+        vector<int> profitNoBlock(prices.size(),0);//手头有股票最大利润
+        vector<int> profitHasBlock(prices.size(),0);//手头没有股票最大利润
+        if(prices.size()==0)
+        return 0;
+        //day0
+        profitNoBlock[0]=0;
+        profitHasBlock[0]=-prices[0];
+        //day1
+        profitNoBlock[1]=max(profitNoBlock[0],profitHasBlock[0]+prices[1]);
+        profitHasBlock[1]=max(profitHasBlock[0],0-prices[1]);
+        for(int i=2;i<prices.size();i++)
+        {
+            profitNoBlock[i]=max(profitNoBlock[i-1],profitHasBlock[i-1]+prices[i]);//把昨天的股票卖掉加上今天的价钱-->没股票了
+            profitHasBlock[i]=max(profitHasBlock[i-1],profitNoBlock[i-2]-prices[i]);//前天卖出，只能今天买入，买入后需减去今天股价
+        }
+    return profitNoBlock[prices.size()-1];
     }
 };
