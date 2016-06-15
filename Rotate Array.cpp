2bbbe1566fb6c2420cecbc7adf0c751bee16f43d
DesmondGao <linchaogao@163.com>
2016-06-15 15:40:54 +0800

    Update Rotate Array.cpp

--- Rotate Array.cpp
+++ Rotate Array.cpp
@@ -14,3 +14,31 @@ public:
         }
     }
 };
+
+//method 2:周期性方法
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        //周期性问题-->%运算符派上了用处
+        if(1==nums.size())
+        {
+            return;
+        }
+        if(k>nums.size())
+        {
+            k=k%nums.size();//周期为数组长度
+        }
+       // vector<int> temp_vector(k);
+        vector<int> result;//不应该设置result长度
+        for(int i=nums.size()-k;i<nums.size();i++)
+        {
+            result.push_back(nums[i]);
+            //reverse(temp_vector.begin(),temp_vector.end());
+        }
+        for(int j=0;j<nums.size()-k;j++)
+        {
+           result.push_back(nums[j]);
+        }
+      nums=result;  
+    }
+};
